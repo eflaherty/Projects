54b37310e48cec6f9bfa9e015735f7e6c75f44e4
Erin Flaherty <eflaherty007@gmail.com>
2024-06-28 08:35:31 -0700

    Removed dead code from Special Elements tests
    Removed code from other test types from Special elements test code

--- SeleniumWebDriver/SpecialElements/EntryPoint.cs
+++ SeleniumWebDriver/SpecialElements/EntryPoint.cs
@@ -123,88 +123,6 @@ class EntryPoint
             Console.WriteLine("Something went wrong!!!");
         }
 
-
-/*         // assert element
-        if (element.Displayed)
-        {
-            GreenMessage("Yes! I can see the element, it's right there!!");
-        }
-        else
-        {
-            RedMessage("Well, something went wrong, I couldn't see the element!");
-        }
-
-        // find element by ID
-        driver.Navigate().GoToUrl(id_url);
-        IWebElement IDElement = driver.FindElement(By.Id(ID));
-
-        // assert element
-        if (IDElement.Displayed)
-        {
-            GreenMessage("Yes! I can see the ID element, it's right there!!");
-        }
-        else
-        {
-            RedMessage("Well, something went wrong, I couldn't see the ID element!");
-        }
-
-        // find element by class name
-        driver.Navigate().GoToUrl(class_url);
-        IWebElement classNameElement = driver.FindElement(By.ClassName(class_name));
-        // Print out the text of the element
-        Console.WriteLine(classNameElement.Text);
-
-        // assert element
-        if (classNameElement.Displayed)
-        {
-            GreenMessage("Yes! I can see the Class Name element, it's right there!!");
-        }
-        else
-        {
-            RedMessage("Well, something went wrong, I couldn't see the Class Name element!");
-        }
-
-        // find element by CSS Path
-        driver.Navigate().GoToUrl(css_url);
-        IWebElement cssPathElement = driver.FindElement(By.CssSelector(css_path));
-
-        // assert elemetn
-        if (cssPathElement.Displayed)
-        {
-            GreenMessage("Yes! I can see the CSS Path element, it's right there!!");
-        }
-        else
-        {
-            RedMessage("Well, something went wrong, I couldn't see the CSS Path element!");
-        }
-
-        // find element by X Path
-        driver.Navigate().GoToUrl(xpath_url);
-
-        // assert element
-        // if (xPathElement.Displayed)
-        // {
-        //     GreenMessage("Yes! I can see the X Path element, it's right there!!");
-        // }
-        // else
-        // {
-        //     RedMessage("Well, something went wrong, I couldn't see the X Path element!");
-        // }
-        
-        // No element found exception
-        try
-        {
-            IWebElement xPathElement = driver.FindElement(By.XPath(x_path));
-            if (xPathElement.Displayed)
-            {
-                GreenMessage("Yes! I can see the X Path element, it's right there!!");
-            }
-        }
-        catch (NoSuchElementException)
-        {
-            RedMessage("Well, something went wrong, I couldn't see the X Path element!");
-        }
-
 /*         IWebElement email = new WebDriverWait(Driver, TimeSpan.FromSeconds(3)).Until(ExpectedConditions.ElementIsVisible(By.Id("email")));
         email.SendKeys("MyName");
         email.Submit(); */
@@ -214,21 +132,6 @@ class EntryPoint
 
 //        textBox = driver.FindElement(By.Name("username"));        
 
-
-
         driver.Quit();
-    }
-
-    private static void GreenMessage(string message)
-    {   
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine(message);
-        Console.ForegroundColor = ConsoleColor.White;
-    }
-    private static void RedMessage(string message)
-    {   
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine(message);
-        Console.ForegroundColor = ConsoleColor.White;
     }
 }
